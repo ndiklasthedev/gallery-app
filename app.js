acf1fa3319d0c3ef9124104059adcf9ca8252379
NdiklasTheCoder <nicholas.v.ndiki@gmail.com>
2020-07-22 01:37:25 +0300

    add views folder

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 
-// Import express package
+// // Import express package
+// const express = require('express');
+
+
+// // Initialize express
+// const app = express();
+
+// // Set up a view engine
+// app.set('view engine', 'ejs');
+
+
+// // Set a static folder
+// app.use(express.static('public'));
+
+
+// // Define the port number
+// const PORT = 5000;
+
+// app.listen(PORT, ()=>{
+//     console.log(`Server is listening on port ${PORT}`)
+// });
+
+
+
 const express = require('express');
 
 
 // Initialize express
 const app = express();
 
+// Set up a view engine
+app.set('view engine', 'ejs');
+
 // Set a static folder
 app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+   res.render('index');
+})
+
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
-})
+});
