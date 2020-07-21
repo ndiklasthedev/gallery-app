9f1aed926aeb4e781be523ee06f2c2055fe9157e
NdiklasTheCoder <nicholas.v.ndiki@gmail.com>
2020-07-22 02:53:46 +0300

    add route

--- app.js
+++ app.js
@@ -23,9 +23,10 @@
 
 
 
+// Import express package
 const express = require('express');
-const upload = require('./upload');
 
+let indexRouter = require('./routes/index');
 
 // Initialize express
 const app = express();
@@ -33,25 +34,12 @@ const app = express();
 // Set up a view engine
 app.set('view engine', 'ejs');
 
+
 // Set a static folder
 app.use(express.static('public'));
 
-
-app.get('/', (req,res)=>{
-    res.render('index');
- })
- 
- // route to handle image upload
- app.post('/upload', (req,res)=>{
-    upload(req,res, (err)=>{
-        if (err){
-            console.log(err)
-        }else{
-            console.log(req.file)
-            res.send('test');
-        }
-    })
- })
+// Define the index router
+app.use('/', indexRouter);
 
 
 // Define the port number
--- upload.js
+++ upload.js
@@ -12,9 +12,24 @@ const storage = multer.diskStorage({
 
 // Initialize upload
 const upload = multer({
-   storage: storage,
-   limits: {fileSize: 1000000},
-}).single('image');
+    storage: storage,
+    limits: {fileSize: 1000000},
+    fileFilter: function(req,file, cb){
+        // Allowed extensions
+        let fileTypes = /jpg|jpeg|png|gif/;
+        // check file extension
+        let extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+        // check mime type
+        let mimetype = fileTypes.test(file.mimetype);
+       
+        if (mimetype && extname){
+            return cb(null, true);
+        }else{
+            cb('Error: Images Only!!')
+        }
+    }
+ }).single('image');
+ 
 
 
 
